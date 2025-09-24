5a747f103cf15f034f02a5e39610399adff2d786
Supraja Tangella <supraja.tangella@gmail.com>
2025-09-24 14:03:56 +0100

    added Careers Page. I created git hub actions. The code should push to azure app service.

--- SampleAzureWebApp/Controllers/HomeController.cs
+++ SampleAzureWebApp/Controllers/HomeController.cs
@@ -18,6 +18,12 @@ namespace SampleAzureWebApp.Controllers
             return View();
         }
 
+        public IActionResult Career()
+        {
+            return View();
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
